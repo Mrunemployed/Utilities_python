473db5fe9d1ff3dacadd1159f26cca64fbc0c77f
Rudra <khan.rudradip@gmail.com>
2024-08-13 22:18:20 +0530

    added readme

--- get_this.py
+++ get_this.py
@@ -9,14 +9,19 @@ def get_this(**kwargs):
         file_map = ([(root,files) for tree in all_dirs for root,dirs,files in os.walk(os.path.abspath(tree),topdown=True)])
         complete_paths = [f"{x}\\{y}" for x,i in file_map for y in i]
         all.extend(complete_paths)
-        check = [x for x in all if re.search(f"{kwargs['like']}\.(jpeg|jpg)$",x)]
+        print(all)
+        if 'ext' in kwargs.keys():
+            check = [x for x in all if re.search(f"{kwargs['file_like']}\.({kwargs['ext']})$",x)]
+        else:
+            check = [x for x in all if re.search(f"{kwargs['file_like']}",x)]
+
         if len(check) > 0:
-            return check[0]
+            return check
         else:
             return False
     else:
         raise Exception("Invalid arguments")
     
-
-foo = get_this(like="mgh")
+# Example:
+foo = get_this(file_like="readme",ext='md')
 print(foo)
---
+++ readme.md
@@ -0,0 +1,11 @@
+# Python utilities
+
+This is a repo containing utilities built using python, these utilities were built in my spare time.
+It can range from stuff that were built just for curiosity to functions that can actually add value to your program.
+
+### get_this.py
+This is a python utility that can fetch the complete path of any file be it within a directory or subdirectories inside a target parent directory.
+- Can resolve the complete path from incomplete filename.
+- Can look into directories and their subdirectories to find the file.
+- Supports regex to further modify the utility to better fit your usecase.
+- Returns a list of matches.
